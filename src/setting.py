e9ff47547ef18b582386e59132d26645c38438e2
YuhaShin <newcrystal1994@gmail.com>
2021-05-30 03:44:52 +0900

    Changed caption

--- src/setting.py
+++ src/setting.py
@@ -30,7 +30,7 @@ rheight = resized_screen.get_height()
 button_offset = 0.25
 
 clock = pygame.time.Clock()
-pygame.display.set_caption("T-Rex Rush by_OldKokiri")
+pygame.display.set_caption("T-Rex Rush by_MilkDragon")
 
 bgm_on=True
 on_pushtime=0
